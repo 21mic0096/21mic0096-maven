c1fc2bf8bea0ad0800d9cee4986accb179a0f2c7
himaj <himaj@Himaja>
2023-11-21 18:54:13 +0530

    hi pipeline 21mic0096

---
+++ pipeline/src/main/java/pipeline/Himajapipeline.java
@@ -0,0 +1,11 @@
+package pipeline;
+
+public class Himajapipeline {
+
+	public static void main(String[] args) {
+		println("System.out.println("HELLO WORLD");
+		// TODO Auto-generated method stub
+
+	}
+
+}
